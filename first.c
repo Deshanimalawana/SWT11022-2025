9897a3b1ec926858bd5351eb0be0a027686c0921
Deshani Malawana <56667876+Deshanimalawana@users.noreply.github.com>
2025-02-08 22:58:18 +0530

    Add files via upload
    First commit

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to Fundamentals of Programming\n");
+    printf("Name: John Doe\n");
+    printf("Registration Number: SEU/IS/XX/ICT/XXX\n");
+    return 0;
+}
